c62c36a4515c9a14c5a65083b46ea88b6760261d
ErgallM <ncwlife@gmail.com>
2011-05-16 18:59:30 +0400

    Добавлен калькулятор АП

---
+++ public/ap.php
@@ -0,0 +1,156 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
+        "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+    <title>Aion Library</title>
+
+    <link rel="stylesheet" href="/css/blueprint/screen.css" type="text/css" media="screen, projection">
+    <link rel="stylesheet" href="/css/blueprint/print.css" type="text/css" media="print">
+    <!--[if lt IE 8]>
+        <link rel="stylesheet" href="/css/blueprint/ie.css" type="text/css" media="screen, projection">
+    <![endif]-->
+
+    <link rel="stylesheet" href="/css/ap.css" type="text/css" media="screen, projection">
+
+    <script language="javascript" src="/js/mootools-core-1.3.2-full-nocompat-yc.js"></script>
+    <script language="javascript" src="/js/Element.serialize.js"></script>
+
+    <script language="javascript" src="/js/main.js"></script>
+</head>
+<body>
+    <div class="container">
+        <h2 align="center">Aion Library &mdash; Калькулятор АП</h2>
+
+        <ul>
+            <li><a href="/">На главную</a></li>
+        </ul>
+
+        <p><label>Текущее количество очков бездны <input type="text" id="now-ap" name="now-ap" value="0" /></label></p>
+
+        <form id="items" method="post">
+        <div class="span-24 last">
+            <div class="span-10">
+                <div class="items">
+                    <img src="/images/ap/icon_1.png" />
+                    Простая древняя икона
+                    <span>(300 ап)</span>
+                    <input type="text" name="items[icon][0]" value="0" />
+                </div>
+                <div class="items">
+                    <img src="/images/ap/icon_2.png" />
+                    Обычная древняя икона
+                    <span>(600 ап)</span>
+                    <input type="text" name="items[icon][1]" value="0" />
+                </div>
+                <div class="items">
+                    <img src="/images/ap/icon_3.png" />
+                    Дорогая древняя икона
+                    <span>(900 ап)</span>
+                    <input type="text" name="items[icon][2]" value="0" />
+                </div>
+                <div class="items">
+                    <img src="/images/ap/icon_4.png" />
+                    Бесценная древняя икона
+                    <span>(1200 ап)</span>
+                    <input type="text" name="items[icon][3]" value="0" />
+                </div>
+            </div>
+            <div class="span-2">&nbsp;</div>
+            <div class="span-10 last">
+                <div class="items">
+                    <img src="/images/ap/seal_1.png" />
+                    Простая древняя печать
+                    <span>(600 ап)</span>
+                    <input type="text" name="items[seal][0]" value="0" />
+                </div>
+                <div class="items">
+                    <img src="/images/ap/seal_2.png" />
+                    Обычная древняя печать
+                    <span>(1200 ап)</span>
+                    <input type="text" name="items[seal][1]" value="0" />
+                </div>
+                <div class="items">
+                    <img src="/images/ap/seal_3.png" />
+                    Дорогая древняя печать
+                    <span>(1800 ап)</span>
+                    <input type="text" name="items[seal][2]" value="0" />
+                </div>
+                <div class="items">
+                    <img src="/images/ap/seal_4.png" />
+                    Бесценная древняя печать
+                    <span>(2400 ап)</span>
+                    <input type="text" name="items[seal][3]" value="0" />
+                </div>
+            </div>
+
+            <hr class="space" />
+
+            <div class="span-10">
+                <div class="items">
+                    <img src="/images/ap/cup_1.png" />
+                    Простая древняя чаша
+                    <span>(1200 ап)</span>
+                    <input type="text" name="items[cup][0]" value="0" />
+                </div>
+
+                <div class="items">
+                    <img src="/images/ap/cup_2.png" />
+                    Обычная древняя чаша
+                    <span>(2400 ап)</span>
+                    <input type="text" name="items[cup][1]" value="0" />
+                </div>
+                <div class="items">
+                    <img src="/images/ap/cup_3.png" />
+                    Дорогая древняя чаша
+                    <span>(3600 ап)</span>
+                    <input type="text" name="items[cup][2]" value="0" />
+                </div>
+                <div class="items">
+                    <img src="/images/ap/cup_4.png" />
+                    Бесценная древняя чаша
+                    <span>(4800 ап)</span>
+                    <input type="text" name="items[cup][3]" value="0" />
+                </div>
+            </div>
+            <div class="span-2">&nbsp;</div>
+            <div class="span-10 last">
+                <div class="items">
+                    <img src="/images/ap/crown_1.png" />
+                    Простая древняя корона
+                    <span>(2400 ап)</span>
+                    <input type="text" name="items[crown][0]" value="0" />
+                </div>
+                <div class="items">
+                    <img src="/images/ap/crown_2.png" />
+                    Обычная древняя корона
+                    <span>(4800 ап)</span>
+                    <input type="text" name="items[crown][1]" value="0" />
+                </div>
+                <div class="items">
+                    <img src="/images/ap/crown_3.png" />
+                    Дорогая древняя корона
+                    <span>(7200 ап)</span>
+                    <input type="text" name="items[crown][2]" value="0" />
+                </div>
+                <div class="items">
+                    <img src="/images/ap/crown_4.png" />
+                    Бесценная древняя корона
+                    <span>(6900 ап)</span>
+                    <input type="text" name="items[crown][3]" value="0" />
+                </div>
+            </div>
+        </div>
+        </form>
+
+        <hr class="space" />
+
+        <div><div class="end">Итогове количество очков бездны после обмена:</div> <span id="end">0</span></div>
+    </div>
+
+    <script language="javascript">
+        window.addEvent('domready', function() {
+            var a = new Ap({'formId':'items', 'resultId': 'end', 'startApId': 'now-ap'});
+        })
+    </script>
+</body>
+</html>
---
+++ public/js/main.js
@@ -0,0 +1,86 @@
+var Ap = new Class({
+    Implements: [Options],
+    options: {
+        /**
+         * Форма
+         */
+        formId: null,
+
+        /**
+         * Начальное количество АП
+         */
+        startApId: null,
+
+        /**
+         * Куда записывать результат
+         */
+        resultId: null,
+
+        /**
+         * Цена итемов
+         */
+        itemsAp: {
+            'icon'  : [300, 600, 900, 1200],
+            'seal'  : [600, 1200, 1800, 2400],
+            'cup'   : [1200, 2400, 3600, 4800],
+            'crown' : [2400, 4800, 7200, 6900]
+        }
+    },
+
+    initialize: function (options) {
+        this.setOptions(options);
+
+        var that = this;
+        $$('#' + this.options.formId + " input[type='text'], #"+ this.options.startApId).addEvents({
+            'focus': function() {
+                var value = Number.from($(this).get('value'));
+                if (!value) $(this).set('value', '');
+            },
+            'blur': function() {
+                var value = Number.from($(this).get('value'));
+                if (!value) $(this).set('value', '0');
+            },
+            'keyup': function() {
+                $(that.options.resultId).set('text', that.calc());
+            },
+            'keypress': function(e) {
+                var e = new Event(e);
+                if(e.code <= 31 || (e.code >= 48 && e.code <= 57) ) {
+                    return true;
+                } else {
+                    e.stopPropagation();
+                    e.stop();
+                    return false;
+                }
+            }
+        });
+
+        return this;
+    },
+
+    calc: function() {
+        if (!$(this.options.formId)) throw new Error("Can't found form with id '" + this.options.formId + "'");
+        
+        var formItems = $(this.options.formId).serialize(true);
+        var allAp = 0;
+        var startAp = ($(this.options.startApId)) ? Number.from($(this.options.startApId).get('value')) : 0;
+
+        if (undefined == formItems.items) return 0;
+
+        formItems = formItems.items;
+        var itemsAp = this.options.itemsAp;
+
+        for (var itemType in formItems) {
+            for (var itemName in formItems[itemType]) {
+                if (undefined != itemsAp[itemType][itemName]) {
+                    var itemCound = formItems[itemType][itemName];
+                    var value = Number.from(itemsAp[itemType][itemName]);
+
+                    allAp += (null != value) ? (itemCound * value) : 0;
+                }
+            }
+        }
+
+        return (allAp + startAp);
+    }
+});
