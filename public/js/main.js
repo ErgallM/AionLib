0ce8e6e846f7352dff98de82700395b044f81ca2
ErgallM <ncwlife@gmail.com>
2011-05-28 19:34:23 +0400

    Допиливаем распределение по группам

--- public/js/main.js
+++ public/js/main.js
@@ -18,6 +18,10 @@ var Ap = new Class({
 
         usersCoundId: null,
 
+        'buttons': {
+            'calcPeople': null
+        },
+
         /**
          * Цена итемов
          */
@@ -28,7 +32,19 @@ var Ap = new Class({
             'crown' : [2400, 4800, 7200, 9600]
         },
 
-        users: []
+        users: [],
+        usersItems: [],
+
+        shablon: {
+            block:  '<div class="group-block"><div class="group-block-title left">{userId}<span>{userAp}</span></div>' +
+                        '<div class="group-block-items right">{items}</div><div class="clear"></div>' +
+            '</div>',
+
+            items:  '<div class="group-block-item"><img src="/images/ap/{itemName}_{itemN}.png" />{itemFullName}' +
+                    '<span>9600 APs</span><div class="clear"></div>' +
+            '</div>'
+
+        }
     },
     startOptions: {},
 
@@ -61,13 +77,15 @@ var Ap = new Class({
             }
         });
 
-        /*
-        $('calcPeople').addEvent('click', function(e) {
-            that.calcPersone();
-            e.stop();
-            return false;
-        });
+        if (this.options.buttons.calcPeople) {
+            this.options.buttons.calcPeople.addEvent('click', function(e) {
+                that.calcPersone();
+                e.stop();
+                return false;
+            });
+        }
 
+        /*
         $('clear').addEvent('click', function(e) {
             that.clear();
             e.stop();
@@ -122,7 +140,11 @@ var Ap = new Class({
             Object.each(value, function(valueItem, valueKey) {
                 var itemAp = itemsAp[key][valueKey];
                 for (var i = 0; i < valueItem; i++) {
-                    stek = Array.append([itemAp], stek);
+                    stek = Array.append([{
+                        ap: itemAp,
+                        name: key,
+                        n: valueKey
+                    }], stek);
                     allPrice += itemAp;
                 }
             });
@@ -131,15 +153,26 @@ var Ap = new Class({
         var users = this.options.users;
         var srez = allPrice / userCound;
 
+        var usersItems = Array.clone(this.options.usersItems);
+
         for (var userId = 0; userId < userCound; userId++) {
-            if (undefined == users[userId]) users[userId] = 0;
+            if (undefined == users[userId]) {
+                users[userId] = 0;
+                usersItems[userId] = [];
+            }
 
             if (users[userId] >= srez) continue;
 
             for (var itemId in stek) {
-                if ((users[userId] + stek[itemId]) <= srez) {
-                    users[userId] += stek[itemId];
-                    stek[itemId] = null;
+                if (stek[itemId]) {
+                    if ((users[userId] + stek[itemId].ap) <= srez) {
+                        users[userId] += stek[itemId].ap;
+
+                        usersItems[userId].append([stek[itemId]]);
+
+                        console.log(stek[itemId], itemId);
+                        stek[itemId] = null;
+                    }
                 }
                 if (users[userId] >= srez) break;
             }
@@ -164,15 +197,24 @@ var Ap = new Class({
         }
 
         stek.each(function(value) {
-            if (!value) return;
-
-            var minUser = getMinUser(users);
-            users[minUser.key] += value;
+            if (value) {
+                var minUser = getMinUser(users);
+                users[minUser.key] += value.ap;
+                if (undefined == usersItems[minUser.key]) usersItems[minUser.key] = [];
+                usersItems[minUser.key].append([value]);
+            }
         });
 
-        console.log(users);
+        console.log(users, usersItems);
+        this.addPerconeResultLine(users, usersItems);
     },
     clear: function() {
         this.setOptions(this.startOptions);
+    },
+    addPerconeResultLine: function(users) {
+        users.each(function(user, key) {
+            var item = this.options.shablon.items;
+            console.log(user, key);
+        });
     }
 });
